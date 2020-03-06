966e4708e00f14edfa8a4726e538204828ca7a8a
PSHald <peters.hald@gmail.com>
2020-03-06 20:51:42 +0100

    Kommentarer

--- src/com/company/RotationTest.java
+++ src/com/company/RotationTest.java
@@ -35,12 +35,11 @@ public class RotationTest extends Applet implements ActionListener, KeyListener 
 
 
     public RotationTest() {
-
         setLayout(new BorderLayout());
         GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
         Canvas3D canvas = new Canvas3D(config);
         transformGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-
+        //step 1 Create a Canvas3D object and add it to the Applet panel.
         add(canvas);
         canvas.addKeyListener(this);
 
@@ -59,20 +58,24 @@ public class RotationTest extends Applet implements ActionListener, KeyListener 
         leftBtn.addKeyListener(this);
         rightBtn.addKeyListener(this);*/
 
+        //step 2 Create a BranchGroup as the root of the scene branch graph.
         BranchGroup scene = CreateScene();
+
+        //Step 5 Call the universe builder utility function
         SimpleUniverse simpleUniverse = new SimpleUniverse(canvas);
         simpleUniverse.getViewingPlatform().setNominalViewingTransform();
         transform.lookAt(GazePoint, ViewerLocation, new Vector3d( 2,1,0));
         transformGroup = simpleUniverse.getViewingPlatform().getMultiTransformGroup().getTransformGroup(0);
+        //step 6 Insert the scene branch graph into the universe builder's Locale.
         simpleUniverse.addBranchGraph(scene);
 
 
-        //Vindue
+        //Vindue for applikation
         MainFrame frame = new MainFrame(this, 1280, 960);
         frame.setTitle("Rotation Test");
         frame.setVisible(true);
     }
-
+    //Næste skridt, sæt X, Y og Z til at være kordinater som rotere bolden, ikke universet.
     private void rotate(){
         Transform3D delta = new Transform3D();
         Vector3d vector = new Vector3d(xLoc, yLoc, zLoc);
@@ -84,6 +87,7 @@ public class RotationTest extends Applet implements ActionListener, KeyListener 
     }
 
     private BranchGroup CreateScene(){
+        //step 2 Create a BranchGroup as the root of the scene branch graph.
         BranchGroup scene = new BranchGroup();
 
         Background background = new Background(new Color3f(0.2f,0.2f,1));
@@ -91,16 +95,26 @@ public class RotationTest extends Applet implements ActionListener, KeyListener 
         background.setApplicationBounds(Bsphere);
         scene.addChild(background);
 
-        transformGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-        scene.addChild(transformGroup);
-
         String localPath = new File("").getAbsolutePath();
         TextureLoader loader = new TextureLoader(localPath+"/src/images/6d6bb0f697c68b30634cf4b38769784838de1c32_full.jpg", "RGB" , new Container());
+        //step 3  Construct a Shape3D node with a TransformGroup node above it.
         Appearance ap = new Appearance();
         ap.setTexture(loader.getTexture());
-
         Sphere sphere = new Sphere(0.2f, Primitive.GENERATE_TEXTURE_COORDS, ap);
-        scene.addChild(sphere);
+        transformGroup.addChild(sphere);
+
+
+        //Step 4 Attach a RotationInterpolator behavior to the TransformGroup
+        // har ikke rigtigt noget formål her og virker stadig uden.
+
+        Transform3D yAxis = new Transform3D();
+        Alpha timing = new Alpha(0, 0);
+        RotationInterpolator nodeRotator = new RotationInterpolator(timing, transformGroup, yAxis, 0.0f, 0.0f);
+        BoundingSphere bounds = new BoundingSphere(new Point3d(0, 0, 0),0);
+        nodeRotator.setSchedulingBounds(bounds);
+        transformGroup.addChild(nodeRotator);
+
+        scene.addChild(transformGroup);
 
         return scene;
     }
